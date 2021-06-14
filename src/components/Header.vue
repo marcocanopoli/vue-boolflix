d7fc585791b78c3aae40b4004a32e956b18c985e
marcocanopoli <canopoli.marco@gmail.com>
2021-06-14 13:07:41 +0200

    assets + npm packages + scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <img src="../assets/img/notflix-logo.png" alt="Not!flix Logo">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        height: 65px;
+        padding: 10px 0;
+
+        img {
+            height: 50%;
+        }
+    }
+</style>
